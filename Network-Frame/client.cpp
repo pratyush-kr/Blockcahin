8e7d2045f2d91be89bee610b3eba1aee24b79569
pratyush-kr <pratyush151198@gmail.com>
2021-10-11 12:39:46 +0530

    Network

--- Network-Frame/client.cpp
+++ Network-Frame/client.cpp
@@ -7,7 +7,27 @@
 
 using namespace std;
 
+struct Block
+{
+    int source;
+    int destination;
+    int ID;
+    string prevHash;
+    string data;
+    int validate; // -1 => validate this hash 0 => validate failed 1 => validated
+    string ownHash;
+    Block(char ID, string prevHash, string data, string ownHash)
+    {
+        this->ID = ID;
+        this->prevHash = prevHash;
+        this->ownHash = ownHash;
+        this->data = data;
+    }
+    Block(){}
+};
+
 void populate(sockaddr_in &, const int);
+int validate(Block &A, const Block &B);
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +39,32 @@ int main(int argc, char *argv[])
     if(fd < 0) {perror("Socket Creation Failed\n");}
     //bind is handeled by OS
     if(connect(fd, (sockaddr*)&saddr, len) < 0) {perror("Connection Failed\n");}
+    char ID;
+    string prevHash, ownHash, data;
+    cout<<"ID: ";
+    cin>>ID;
+    cout<<"PrevHash: ";
+    cin>>prevHash;
+    cout<<"OwnHash: ";
+    cin>>ownHash;
+    cout<<"Data: ";
+    cin>>data;
+    Block B(ID, prevHash, data, ownHash);
+    B.ID = stoi(argv[1]);
+    B.destination = stoi(argv[2]);
+    Block R;
+    while(1)
+    {
+        B.source = B.ID;
+        B.destination = B.ID + 1;
+        cout<<"Sending Own Hash:\n";
+        send(fd, &B, sizeof(Block), 0);
+        cout<<"Recieving Previous Hash:\n";
+        recv(fd, &R, sizeof(Block), 0);
+        cout<<"Validating Previous Hash:\n";
+        validate(R, B);
+        cout<<"Sending Previous Hash to server: ";
+    }
     return 0;
 }
 
--- Network-Frame/server.cpp
+++ Network-Frame/server.cpp
@@ -1,8 +1,46 @@
 #include<iostream>
 #include<stdlib.h>
 #include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<unistd.h>
 
-int main()
+using namespace std;
+
+struct Block
 {
+    int source;
+    int destination;
+    int ID;
+    string prevHash;
+    string data;
+    int validate; // -1 => validate this hash 0 => validate failed 1 => validated
+    string ownHash;
+    Block(char ID, string prevHash, string data, string ownHash)
+    {
+        this->ID = ID;
+        this->prevHash = prevHash;
+        this->ownHash = ownHash;
+        this->data = data;
+    }
+    Block(){}
+};
+
+void populate(sockaddr_in &, const int);
+
+int main(int argc, char *argv[])
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0) {perror("Socket Creation Failed\n");}
+    sockaddr_in saddr, caddr;
+    int pno = stoi(argv[1]);
+    populate(saddr, pno);
     return 0;
+}
+
+void populate(sockaddr_in &addr, const int pno)
+{
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(pno);
+    addr.sin_addr.s_addr = INADDR_ANY;
 }
